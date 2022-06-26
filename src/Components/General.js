75de894272a5e2cc76fdcc4aa4a8ad1fe5a2fd86
Seth Feldman <namdlefhtes@gmail.com>
2022-06-26 10:36:06 -0400

    programatic field data

--- src/Components/General.js
+++ src/Components/General.js
@@ -6,48 +6,41 @@ class General extends React.Component {
     constructor(props){
         super(props);
     }
-
+    
     render() {
         const { data, handleChange, handleSubmit, handleEdit } = this.props;
+        const formFields = Object.entries(data.general).slice(0, -1);
 
         return (
         <section>
-            <h1>General</h1>
+             <h1>General</h1>
             <form id= "general" onSubmit={(event)=>handleSubmit(event)}>
-                <label>Name: </label>
-                {!data.general.saved ? 
-                    <input
-                        required
-                        name="name"
-                        type="text"
-                        value= {data.general.name}
-                        onChange={(event) => handleChange(event)}
-                    >
-                    </input> : <p>{data.general.name}</p> }
-                
-                <label>Email</label>
-                {!data.general.saved ? 
-                    <input
-                        required
-                        name="email"
-                        type="text"
-                        value= {data.general.email}
-                        onChange={(event)=> handleChange(event)}
-                    >
-                    </input> : <p>{data.general.email}</p> }
-                
-                <label>Phone</label>
-                {!data.general.saved ?
-                    <input
-                        required
-                        name="phone"
-                        type="tel"
-                        value= {data.general.phone}
-                        onChange={(event)=> handleChange(event)}
-                    >
-                    </input>
-                : <p>{data.general.phone}</p>
+                { formFields.map((item)=>{
+                    // item consists of name of field ((0) and field val (1)
+                    return (
+
+                    !data.general.saved ?   
+                    <>
+                        <label>{`${item[0][0].toUpperCase()}${item[0].substring(1)}: `}</label>
+                        <input
+                            required
+                            name = {item[0]}
+                            type = {item[0] == "phone" ? "tel" : "text"}
+                            value = {item[1]}
+                            onChange={(event) => handleChange(event)}
+                        >
+                        </input>
+                    </>
+                    :
+                    <>
+                        <label>{`${item[0][0].toUpperCase()}${item[0].substring(1)}: `}</label>
+                        <p>{item[1]}</p>
+                    </>
+
+                    )
+                })
                 }
+                
                 {!data.general.saved ? 
                     <input type="submit" value="Submit"></input>
                     :
